cc8540dca0a1108144093d69759d992601818330
drheinrich940 <drheinrich940@gmail.com>
2018-05-28 13:45:23 +0200

    from sklearn.preprocessing import StandardScaleefrom sklearn.preprocessing import StandardScafrom sklearn.preprocessing import StandardScalerfrom sklearn.preprocessing import StandardScalerler

--- preprocessing.py
+++ preprocessing.py
@@ -5,3 +5,18 @@ Created on Mon May 28 13:27:56 2018
 @author: Henri
 """
 
+import numpy as np
+import pandas as pd
+import load_data as ld
+from sklearn.preprocessing import StandardScaler
+
+def buildDataSet():
+    datas = ld.loadDatas()
+    return datas.drop(columns = ['label'])
+    
+def buildTargets():
+    return ld.colonne('label')
+
+def preprocess(datas):
+    scaler = StandardScaler().fit(datas)
+    return scaler.transform(datas)
