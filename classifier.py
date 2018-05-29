24660d8afec1ed956083fd976618fd9447eff908
drheinrich940 <drheinrich940@gmail.com>
2018-05-29 13:12:58 +0200

    Classifier class, classifier chooser, hyper parameters

--- classifier.py
+++ classifier.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 28 13:48:07 2018
-
-@author: Henri
-"""
-
 #Here are the different imports for the cross validation:
 
 #from sklearn.model_selection import cross_val_score
@@ -22,9 +15,10 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn import neighbors, datasets
 from sklearn.neighbors import KNeighborsClassifier
-
+#from sklearn.neural_network import MLPClassifier
+# NOT WORKING from sklearn.gaussian_process import GaussianProcessClassifier
+#import hyper_param as hp
 import numpy as np
-from process import process
 import pickle
 RANDOM_STATE = 123
 
@@ -34,20 +28,15 @@ class Classifier(RandomForestClassifier):
         pass
 
     def fit(self, X, y):
-        pro = process(X, y)
-        a = pro.getFeatures()
-        b = pro.getLabels()
-        self.clf = RandomForestClassifier(n_jobs = -1, n_estimators = 100, warm_start=True, max_features=None,
+        self.clf = RandomForestClassifier(n_jobs = -1, n_estimators = 1000, warm_start=True, max_features=None,
                                oob_score=True,
                                random_state= RANDOM_STATE)
-        self.clf.fit(a, b)
+        self.clf.fit(X, y)
 
     def predict(self, X):
-	X = process(X)
         return self.clf.predict(X)
 
     def predict_proba(self, X):
-	X = process(X)
         return self.clf.predict_proba(X) # The classes are in the order of the labels returned by get_classes
         
     def get_classes(self):
@@ -62,43 +51,29 @@ class Classifier(RandomForestClassifier):
 
 
     def  best_fit(self,names, classifiers, x, y):
-	
-        pro = process(x, y)
-        a = pro.getFeatures()
-        b = pro.getLabels()
+        score_boucle_train = []
+        final_array = []
+        best=["",0]
+        for name, clf in zip(names, classifiers):
+            score_boucle_train.append( cross_val_score(clf, x, y, cv=10, n_jobs=1 ))
 
-    	score_boucle_train = []
-    	final_array = []
-    	best=["",0]
+        final_array=np.mean(score_boucle_train, axis=1, dtype=np.float64) 
 
-    	for name, clf in zip(names, classifiers):
-        	score_boucle_train.append( cross_val_score(clf, a, b, cv=10, n_jobs=1 ))
-
-    	final_array=np.mean(score_boucle_train, axis=1, dtype=np.float64) 
-
-    	for score, name in zip(final_array, names):
-        
-        	if score>best[1] :
-            		best[1]=score
-            		best[0]=name
-    	return best
+        for score, name in zip(final_array, names):
+            if score>best[1] :
+                best[1]=score
+                best[0]=name
+        return best
 
 	
     def  return_final_array(self,names, classifiers, x, y):
-	
-        pro = process(x, y)
-        a = pro.getFeatures()
-        b = pro.getLabels()
-
-    	score_boucle_train = []
-    	final_array = []
-  	best=["",0]
-
-    	for name, clf in zip(names, classifiers):
-        	score_boucle_train.append( cross_val_score(clf, a, b, cv=10, n_jobs=1 ))
-
-    	final_array=np.mean(score_boucle_train, axis=1, dtype=np.float64) 
-	return final_array
+        score_boucle_train = []
+        final_array = []
+        best=["",0]
+        for name, clf in zip(names, classifiers):
+            score_boucle_train.append( cross_val_score(clf, x, y, cv=10, n_jobs=1 ))
+        final_array=np.mean(score_boucle_train, axis=1, dtype=np.float64) 
+        return final_array
 
 
 
--- preprocessing.py
+++ preprocessing.py
@@ -5,14 +5,21 @@ Created on Mon May 28 13:27:56 2018
 @author: Henri
 """
 
-import numpy as np
-import pandas as pd
 import load_data as ld
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 def buildDataSet():
     datas = ld.loadDatas()
-    return datas.drop(columns = ['label'])
+    datas = datas.drop( columns =['label'])
+    return datas
+
+
+
+def divide(x, y):
+    X_train, X_test, Y_train, Y_test = train_test_split(x,y, test_size = 0.2, random_state = 42)
+    print(X_train, X_test, Y_train, Y_test)
+    return X_train, X_test, Y_train, Y_test
     
 def buildTargets():
     return ld.colonne('label')
