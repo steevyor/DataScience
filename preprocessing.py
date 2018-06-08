9704cfa06006ea432b432ddcee2ebf11c6e10a73
drheinrich940 <drheinrich940@gmail.com>
2018-06-08 21:18:09 +0200

    last mod

--- preprocessing.py
+++ preprocessing.py
@@ -19,7 +19,7 @@ def buildDataSet():
 #devide the dataset in order to test and process on a part off the dataset
 def divide(x, y):
     X_train, X_test, Y_train, Y_test = train_test_split(x,y, test_size = 0.2, random_state = 42)
-    print(X_train, X_test, Y_train, Y_test)
+    #print(X_train, X_test, Y_train, Y_test)
     return X_train, X_test, Y_train, Y_test
 
 
